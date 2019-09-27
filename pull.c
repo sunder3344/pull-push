5993137f4aa5e3336e682fe93e5e6248cfcb0629
b <b@10.61.19.12>
2019-09-27 17:13:45 +0800

    update

--- pull.c
+++ pull.c
@@ -22,11 +22,23 @@
 #define LISTENQ 2048
 #define SOCKET_NUM 4096
 #define INFTIM 1000
+#define RNDLEN 10
+#define NUMBER 10
 #define SEP "===sunder==="
 
 char filename[300];
 int ffd, filesize;
 char *path;
+
+typedef struct File_st {
+	char filename[300];
+	char rnd[RNDLEN];
+	int filesize;
+	int ffd;
+} FILE_ST;
+
+//FILE_ST *ft = (FILE_ST *)malloc(sizeof(FILE_ST) *  NUMBER); 
+FILE_ST *ft; 
 
 int isFileName(char *str) {
 	char *n = strstr(str, SEP);
@@ -38,22 +50,67 @@ int isFileName(char *str) {
 }
 
 int strchar(char *str, char ch) {
+        int i;
+        int count = -1;
+        int length = strlen(str);
+        for (i = 0; i < length; i++) {
+                if (str[i] == ch) {
+                        count = i;
+                        break;
+                }
+        }
+        return count;
+}
+
+void packft(char *name, char *rnd, int filesize, int ffd) {
+	//pack the file info into ft
+	int i = 0;
+	int flag = 0;
+	for (i = 0; i < NUMBER; i++) {
+		if (strlen(ft[i].rnd) == 0 && strlen(ft[i].filename) == 0) {
+			strcpy(ft[i].rnd, rnd);
+			strcpy(ft[i].filename, name);
+			ft[i].filesize = filesize;
+			ft[i].ffd = ffd;
+			flag = 1;
+			break;	
+		} else {
+			continue;
+		}
+	}
+	if (flag == 0) {
+		printf("Sorry! We only support %d files simultaneously!\n", NUMBER);
+		exit(2);
+	}
+	/*for (i = 0; i < NUMBER; i++) {
+		printf("name = %s, rnd = %s, size = %d, ffd = %d\n", ft[i].filename, ft[i].rnd, ft[i].filesize, ft[i].ffd);
+	}*/
+	return;
+}
+
+FILE_ST findft(char *rnd) {
 	int i;
-	int count = -1;
-	int length = strlen(str);
-	for (i = 0; i < length; i++) {
-		if (str[i] == ch) {
-			count = i;
+	FILE_ST tmp;
+	for (i = 0; i < NUMBER; i++) {
+		if (strcmp(ft[i].rnd, rnd) == 0) {
+			return (FILE_ST)ft[i];
 			break;
 		}
 	}
-	return count;
+	return tmp;
 }
 
 void createFile(char *str) {
 	char *p = strstr(str, SEP);
-	char *tmp = p + strlen(SEP);
+	char *tmp2 = p + strlen(SEP);
+	//printf("str = %s, tmp2 = %s\n", str, tmp2);
+	char *p2 = strstr(tmp2, SEP);
+	char *tmp = (char *)malloc(sizeof(char) * 200);
+	strncpy(tmp, tmp2, strlen(tmp2) - strlen(SEP) - RNDLEN);
+	char *rnd = p2 + strlen(SEP);
+
 	filesize = atoi(tmp);
+	//file name(without path)
 	char *name = (char *)malloc(sizeof(char) * 100);
 	char tmpfilename[300];
 	memset(&filename, 0, sizeof(filename));
@@ -61,16 +118,19 @@ void createFile(char *str) {
 
 	int z = strlen(str) - strlen(p);
 	strncpy(name, str, z);
+	name[sizeof(name)] = '\0';
 
 	int pos = strchar(name, '.');
-	char *pre_name = (char *)malloc(sizeof(char) * 80);
-	char *suffix = (char *)malloc(sizeof(char) * 20);
-	if (pos > 0) {          //存在后缀名的文件
-		strncpy(pre_name, name, pos);
-		strncpy(suffix, name + pos, strlen(name) - pos);
-	}
-	//printf("suffix = %s\n", suffix);
-	//printf("pre_name = %s\n", pre_name);
+        char *pre_name = (char *)malloc(sizeof(char) * 80);
+        char *suffix = (char *)malloc(sizeof(char) * 20);
+        if (pos > 0) {          //存在后缀名的文件
+                strncpy(pre_name, name, pos);
+		pre_name[sizeof(pre_name)] = '\0';
+                strncpy(suffix, name + pos, strlen(name) - pos);
+		suffix[sizeof(suffix)] = '\0';
+        }
+        //printf("suffix = %s\n", suffix);	
+        //printf("pre_name = %s\n", pre_name);
 
 	//whether last char of path is /
 	strcat(filename, path);	
@@ -88,8 +148,8 @@ void createFile(char *str) {
 		strftime(now, sizeof(now), "%Y%m%d%H%M%S", at);
 		strcat(tmpfilename, path);
 		if (path[strlen(path) - 1] != '/') {
-			strcat(tmpfilename, "/");
-		}
+                	strcat(tmpfilename, "/");
+        	}
 		strcat(tmpfilename, pre_name);
 		strcat(tmpfilename, "_");
 		strncat(tmpfilename, now, strlen(now));
@@ -98,12 +158,18 @@ void createFile(char *str) {
 		//printf("after filename = %s\n", filename);
 		//printf("tmpfilename=%s\n", tmpfilename);
 		strcpy(filename, tmpfilename);
+		filename[sizeof(filename)] = '\0';
 	}
 	ffd = open(filename, O_CREAT | O_RDWR, 0666);
 	if (ffd < 0) {
 		perror("open file error!");
 		exit(1);
-	}	
+	}
+	//packft(name, rnd, filesize, ffd);
+	free(name);
+	free(tmp);
+	free(pre_name);
+	free(suffix);
 	return;
 }
 
@@ -116,7 +182,9 @@ int main(int argc, char * argv[]) {
 	int nfds;
 	int i;
 	int sock_fd, conn_fd;
-	char buf[MAXLINE];
+	char buf[MAXLINE + RNDLEN];
+	char bufdup[MAXLINE];
+	ft = (FILE_ST *)malloc(sizeof(FILE_ST) *  NUMBER); 
 
 	if (argc != 3) {
 		perror("param error!");
@@ -154,8 +222,6 @@ int main(int argc, char * argv[]) {
 				clilen = sizeof(struct sockaddr_in);
 				conn_fd = accept(listenfd, (struct sockaddr *)&clientaddr, &clilen);
 				printf("accept a new client: %s\n", inet_ntoa(clientaddr.sin_addr));
-				/*char filename[300] = "2.jpg";
-				ffd = open(filename, O_CREAT | O_RDWR, 0666);*/
 				ev.data.fd = conn_fd;
 				ev.events = EPOLLIN;		//设置监听事件为可写
 				epoll_ctl(epfd, EPOLL_CTL_ADD, conn_fd, &ev);	//新增套接字
@@ -164,7 +230,7 @@ int main(int argc, char * argv[]) {
 					continue;
 				}
 				memset(&buf, 0, sizeof(buf));
-				if ((n = recv(sock_fd, buf, MAXLINE, 0)) < 0) {
+				if ((n = recv(sock_fd, buf, MAXLINE + RNDLEN, 0)) < 0) {
 					if (errno == ECONNRESET) {
 						close(sock_fd);	
 						events[i].data.fd = -1;
@@ -175,7 +241,7 @@ int main(int argc, char * argv[]) {
 				} else if (n == 0) {
 					close(sock_fd);
 					close(ffd);
-					printf("End...\n");
+					printf("End... %s finished\n", filename);
 					continue;	
 					events[i].data.fd = -1;
 				}
@@ -185,12 +251,13 @@ int main(int argc, char * argv[]) {
 					createFile(buf);
 				} else {
 					lseek(ffd, 0, SEEK_END);
-					int res = write(ffd, buf, n);
-					if (res != n) {
-						perror("transfer errors!");
-						exit(1);
-					}
+                                        int res = write(ffd, buf, n);
+                                        if (res != n) {
+                                                perror("transfer errors!");
+                                                exit(1);
+                                        }
 				}
+				
 				ev.data.fd = sock_fd;
 				ev.events = EPOLLOUT;
 				epoll_ctl(epfd, EPOLL_CTL_MOD, sock_fd, &ev);	//改动监听事件为可读
@@ -204,5 +271,6 @@ int main(int argc, char * argv[]) {
 			}
 		}
 	}
+	free(ft);
 	return 0;
 }
--- push.c
+++ push.c
@@ -14,84 +14,137 @@
 #include <mqueue.h>
 #include <sys/mman.h>
 #include <sys/select.h>
+#include <time.h>
+
 #define SEP "===sunder==="
 #define MAXLINE 4096
+#define RANDNUM 10
+#define random(x) (rand()%x)
+
+char *getrandstr(int num) {
+        int i;
+        char arr[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 
+                      'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B',
+                      'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U',
+                      'V', 'W', 'X', 'Y', 'Z'
+                     }; 
+        char *result = (char *)malloc(sizeof(char) * num);
+        int len = sizeof(arr) - 1;
+        srand((int)time(0));
+        for (i = 0; i < num; i++) {
+                int r = random(len);
+                result[i] = arr[r];
+        }
+        return result;
+}
 
 char* strrev(char *s) {
 	char *h = s;
 	char* t = s;  
-	char ch;
+        char ch;
 	while(*t++){};  
-	t--;
-	t--;
+        t--;
+        t--;
+
 	while(h < t) {  
-		ch = *h;
-		*h++ = *t;    /* h向尾部移动 */
-		*t-- = ch;    /* t向头部移动 */
-	}
-	return(s);
+            ch = *h;  
+            *h++ = *t;    /* h向尾部移动 */  
+            *t-- = ch;    /* t向头部移动 */  
+        }  
+       
+        return(s);  
 }
 
 int strchar(char *str, char ch) {
-	int i;
-	int count = -1;
-	int length = strlen(str);
-	for (i = 0; i < length; i++) {
-			if (str[i] == ch) {
-					count = i;
-					break;
-			}
-	}
-	return count;
+        int i;
+        int count = -1;
+        int length = strlen(str);
+        for (i = 0; i < length; i++) {
+                if (str[i] == ch) {
+                        count = i;
+                        break;
+                }
+        }
+        return count;
 }
 
-char * fileStrDeal(char *filename, char *filestr) {
+int getfilesize(char *filename) {
+	struct stat statbuff;
+        if (stat(filename, &statbuff) < 0) {
+                perror("file error\n");
+                exit(1);
+        }
+	return statbuff.st_size;
+}
+
+char * fileStrDeal(char *filename, char *filestr, char *rnd) {
 	//发送文件名及文件大小
 	//filestr = (char *)malloc(sizeof(char) * 300);
 	unsigned long filesize = -1;
-	char size[100];
-	struct stat statbuff;
-	if (stat(filename, &statbuff) < 0) {
-		perror("file error\n");
-		exit(1);
-	} else {
-		filesize = statbuff.st_size;
-		sprintf(size, "%lld", filesize);
-	}
-	strcpy(filestr, filename);
+        char size[100];
+        struct stat statbuff;
+        if (stat(filename, &statbuff) < 0) {
+                perror("file error\n");
+                exit(1);
+        } else {
+                filesize = statbuff.st_size;
+                sprintf(size, "%lld", filesize);
+        }
+        strcpy(filestr, filename);
 	//剔除所有路径斜杠
 	int pos_rev = strchar(strrev(strdup(filestr)), '/');
-	int pos = strlen(filestr) - pos_rev;
 	char *realname = (char *)malloc(sizeof(char) * 100);
-	strcpy(realname, filestr + pos);
+	if (pos_rev != -1) {
+		int pos = strlen(filestr) - pos_rev;
+		strcpy(realname, filestr + pos);
+	} else {
+		strcat(realname, filestr);
+	}
 
+        strcat(realname, SEP);
+        strcat(realname, size);
 	strcat(realname, SEP);
-	strcat(realname, size);
+	strcat(realname, rnd);
 	memset(&filestr, 0, sizeof(filestr));
 	filestr = realname;
 	return filestr;
 }
 
-void sleep_ms(int msec) {
-	struct timeval timeout = {0, msec};	
+void sleep_ms(unsigned int sec) {
+	struct timeval tval;
+	tval.tv_sec = sec / 1000;
+	tval.tv_usec = (sec * 1000) % 1000000;
+	select(0, NULL, NULL, NULL, &tval);
 }
 
 int main(int argc, char * argv[]) {
 	int sockfd;
 	int new_fd;
+	int old_percent;
+	int total_size = 0;
+	int step = 5;
 	char *filename;
 	char *filestr = (char *)malloc(sizeof(char) * 300);
-	char buff[MAXLINE];
 	char buffer[MAXLINE];
 	struct sockaddr_in addr;
 
-	if (argc != 4) {
+	if (argc < 4) {
 		perror("the parameters should be host port file");
 		exit(1);
 	}
 
 	int port = atoi(argv[2]);
 	filename = argv[3];
+	char *percent_show = argv[4];
+	if (percent_show == NULL) {
+		percent_show = "";
+	}
+	if (strcmp(percent_show, "-o") == 0) {
+		step = 1;
+	}
+	
+	//文件大小
+	int file_size = getfilesize(filename);
 	
 	addr.sin_family = AF_INET;
 	addr.sin_port = htons(port);
@@ -102,21 +155,23 @@ int main(int argc, char * argv[]) {
 	
 	//socket延迟关闭
 	struct linger so_linger;
-	so_linger.l_onoff = 1;
-	so_linger.l_linger = 10;
+        so_linger.l_onoff = 1;
+        so_linger.l_linger = 10;
 	setsockopt(sockfd, SOL_SOCKET, SO_LINGER, &so_linger, sizeof(so_linger));
 
 	while (connect(sockfd, (struct sockaddr *)&addr, sizeof(struct sockaddr)) == -1);
 
+	
+	char *rnd = getrandstr(RANDNUM);
 	//发送文件名及文件大小
-	filestr = fileStrDeal(filename, filestr);
+	filestr = fileStrDeal(filename, filestr, rnd);
 	int send_size = send(sockfd, filestr, strlen(filestr), 0);
 	if (send_size < 0) {
 		perror("transfer file failed!");
 		exit(1);
 	}
-	sleep(1);
-	
+	//sleep(1);
+	sleep_ms(100);
 	
 	//先发送文件名及文件大小
 	//接受对方返回是否准备好
@@ -133,11 +188,25 @@ int main(int argc, char * argv[]) {
 				perror("transfer file error!");
 				exit(1);
 			}
+			//显示上传百分比
+			total_size += read_size;
+			int percent = total_size * 100 / file_size;
+			if (old_percent != percent) {
+				if (percent % step == 0) {
+					int j = 0;
+					for (j = 0; j < percent; j++) {
+						printf("=");
+					}
+					printf(">%d%%\n", percent);
+				}
+			}
+			old_percent = percent;
 			bzero(buffer, sizeof(buffer));
 		}
 		fclose(fp);
 	}
 	printf("----------transfer ok---------\n");
+	free(filestr);
 	close(sockfd);
 	return 0;
 }
